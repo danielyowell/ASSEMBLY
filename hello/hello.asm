8220b3f6b92bd50d6973c700ac293919b524ee1a
“Daniel <danielyowell@ou.edu>
2023-12-12 18:52:48 -0600

    simple and clean

--- hello/hello.asm
+++ hello/hello.asm
@@ -1,5 +1,5 @@
 section .data
-    hello db 'Hello, Worlb!',0
+    hello db 'Hello, World!', 0xA  ; add newline character
 
 section .text
     global _start
@@ -9,7 +9,7 @@ _start:
     mov eax, 4          ; syscall: write
     mov ebx, 1          ; file descriptor: stdout
     mov ecx, hello      ; pointer to the message
-    mov edx, 13         ; length of the message
+    mov edx, 14         ; length of the message including the newline character
     int 0x80            ; call kernel
 
     ; exit the program
